2c3a22dd52e4a8ea106008f5de6281b555fc5d75
Jesse Luoto <jesse.luoto@gmail.com>
2022-12-18 09:43:20 +0200

    Add more puning

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ yes | docker pull ghcr.io/jehna/pentobox:main
 yes | docker image prune > /dev/null
 yes | docker container prune > /dev/null
 yes | docker volume prune > /dev/null
+yes | docker system prune > /dev/null
 
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + "$IP" > /dev/null
